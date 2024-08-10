1219ab98ea24431f0aac99401d87631ab7e5eee5
Jaskirat Oberoi <jaskirat@Jaskirats-MacBook-Pro.local>
2024-08-11 00:28:08 +0530

    Added Schedule info with mui tabs

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import Hero from "./components/Hero/Hero";
 import Navbar from "./components/Navbar/Navbar";
 import Registration from "./components/Registration/Registration";
-
+import ColorTabs from "./components/Schedule/Schedule";
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Hero />
       <Registration />
+      <ColorTabs />
     </div>
   );
 }
---
+++ src/components/Schedule/Schedule.jsx
@@ -0,0 +1,66 @@
+import React from "react";
+import PropTypes from "prop-types";
+import Tabs from "@mui/material/Tabs";
+import Tab from "@mui/material/Tab";
+import Box from "@mui/material/Box";
+
+function CustomTabPanel(props) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+    >
+      {value === index && <Box sx={{ p: 3 }}>{children}</Box>}
+    </div>
+  );
+}
+
+CustomTabPanel.propTypes = {
+  children: PropTypes.node,
+  index: PropTypes.number.isRequired,
+  value: PropTypes.number.isRequired,
+};
+
+export default function ColorTabs() {
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  return (
+    <>
+      <Box sx={{ width: "100%" }} className="schedule">
+        <Box
+          sx={{ borderBottom: 1, borderColor: "divider" }}
+          className="schedule-tabs"
+        >
+          <h1 className="schedule-title  orbitron-font-set">Schedule</h1>
+          <Tabs
+            value={value}
+            onChange={handleChange}
+            aria-label="conference schedule tabs"
+          >
+            <Tab label="CME" />
+            <Tab label="Day One" />
+            <Tab label="Day Two" />
+          </Tabs>
+        </Box>
+        <CustomTabPanel value={value} index={0}>
+          CME Schedule
+        </CustomTabPanel>
+        <CustomTabPanel value={value} index={1}>
+          Day One Schedule
+        </CustomTabPanel>
+        <CustomTabPanel value={value} index={2}>
+          Day Two Schedule
+        </CustomTabPanel>
+      </Box>
+    </>
+  );
+}
